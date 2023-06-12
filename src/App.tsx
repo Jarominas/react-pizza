e374ebdf6c39b9a233f8818660f98c654941d1b3
Artur Jarominas <artur.jarominas@gmail.com>
2023-06-12 19:21:11 +0300

    fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import './scss/app.scss'
+import { useState, useEffect } from 'react'
+
 import Header from './components/Header'
 import Categories from './components/Categories'
 import Sort from './components/Sort'
 import PizzaBlock from './components/PizzaBlock'
-import pizzaData from '../src/pizza.json'
+// const url = 'https://6486e8e2beba6297278f7688.mockapi.io/items'
 
 function App() {
+      const [pizzas, setPizzas] = useState([])
+
+      const getPizzas = async () => {
+            const response = await fetch(
+                  'https://6486e8e2beba6297278f7688.mockapi.io/items'
+            )
+            const pizzas = await response.json()
+            setPizzas(pizzas)
+      }
+
+      useEffect(() => {
+            getPizzas()
+      }, [])
       return (
             <div className='wrapper'>
                   <Header />
@@ -17,7 +32,7 @@ function App() {
                               </div>
                               <h2 className='content__title'>All Pizzas</h2>
                               <div className='content__items'>
-                                    {pizzaData.map((pizza) => (
+                                    {pizzas.map((pizza) => (
                                           <PizzaBlock pizza={{ ...pizza }} />
                                     ))}
                               </div>
