bd080800a7925d59bb4a6e204829e2004c2bb3e7
Artur Jarominas <artur.jarominas@gmail.com>
2023-06-21 16:54:03 +0300

    work with redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import counterReducer from './slice/counterSlice'
 
 export const store = configureStore({
-      reducer: {},
+      reducer: {
+            counter: counterReducer,
+      },
 })
