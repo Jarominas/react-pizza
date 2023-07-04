1fa50c955cbef560e1a219af0d7f616789bd0c82
Artur Jarominas <artur.jarominas@gmail.com>
2023-07-04 12:36:53 +0300

    work with fullpizza page

--- src/components/PizzaBlock.tsx
+++ src/components/PizzaBlock.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { addItem } from '../redux/slices/cartSlice'
+import { Link } from 'react-router-dom'
 
 const PizzaBlock = ({ pizza }) => {
       const dispatch = useDispatch()
@@ -35,12 +36,14 @@ const PizzaBlock = ({ pizza }) => {
       }
       return (
             <div className='pizza-block'>
-                  <img
-                        className='pizza-block__image'
-                        src={imageUrl}
-                        alt='Pizza'
-                  />
-                  <h4 className='pizza-block__title'>{title}</h4>
+                  <Link to={`/fullpizza/${pizza.id}`}>
+                        <img
+                              className='pizza-block__image'
+                              src={imageUrl}
+                              alt='Pizza'
+                        />
+                        <h4 className='pizza-block__title'>{title}</h4>
+                  </Link>
                   <div className='pizza-block__selector'>
                         <ul>
                               {types.map((type, id) => (
